5f33233e6c89caab895043d063ec2aad5ec3f45b
Alex Kleiman <alexjkleiman@gmail.com>
2024-02-24 18:05:40 -0500

    Implemented my custom plugin

--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -1,13 +1,13 @@
+-- Note: this plugin is a modified version of echasnovski's mini.starter plugin. I didn't like the keybinding, and wanted to add some compatibility with neotree, so I edited it and uploaded it to my nvim configuration repository (kle1man/nvim) so that I could use it through lazy. Most of the credit goes to him, he's a wonderful developer.
+
 return {
--- 	"echasnovski/mini.starter",
--- 	version = "*",
--- 	config = function()
--- 		require("mini.starter").setup({
--- 			header = "kle1man/nvim", -- changes default greeting
--- 			footer = "", -- hides help section
--- 			silent = true, -- hides query keystrokes
--- 		})
--- 		-- vim.cmd([[autocmd VimEnter * silent Neotree]])
--- 		-- vim.cmd([[autocmd VimEnter * silent wincmd p]])
--- 	end,
+	"kle1man/nvim",
+	requires = "custom/starter",
+	config = function()
+		require("custom.starter").setup({
+			header = "kle1man/nvim", -- changes default greeting
+			footer = "", -- hides help section
+			silent = true, -- hides query keystrokes
+		})
+	end,
 }
