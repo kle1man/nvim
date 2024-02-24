f049895414e7a3664c328bd15edebfdde0340868
Alex Kleiman <alexjkleiman@gmail.com>
2024-02-24 14:45:47 -0500

    Changed ensure_installed to auto_install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup{
-            ensure_installed = {"lua", "gitignore", "java", "latex", "markdown_inline", "python"},
+            auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
         }
