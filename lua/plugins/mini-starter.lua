e1c0dfb13a3d76b14446bdcf3107c9c636fc6266
Alex Kleiman <alexjkleiman@gmail.com>
2024-02-24 18:06:52 -0500

    Made lazy work with the new plugin file structure

--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -1,8 +1,7 @@
 -- Note: this plugin is a modified version of echasnovski's mini.starter plugin. I didn't like the keybinding, and wanted to add some compatibility with neotree, so I edited it and uploaded it to my nvim configuration repository (kle1man/nvim) so that I could use it through lazy. Most of the credit goes to him, he's a wonderful developer.
 
 return {
-	"kle1man/nvim",
-	requires = "custom/starter",
+	"kle1man/nvim/custom",
 	config = function()
 		require("custom.starter").setup({
 			header = "kle1man/nvim", -- changes default greeting
