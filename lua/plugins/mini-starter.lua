ecd0aeb67e5ee54a6dade983ff29a34da0f2e913
Alex Kleiman <alexjkleiman@gmail.com>
2024-02-24 18:09:45 -0500

    Fixed errors

--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -1,7 +1,8 @@
 -- Note: this plugin is a modified version of echasnovski's mini.starter plugin. I didn't like the keybinding, and wanted to add some compatibility with neotree, so I edited it and uploaded it to my nvim configuration repository (kle1man/nvim) so that I could use it through lazy. Most of the credit goes to him, he's a wonderful developer.
 
 return {
-	"kle1man/nvim/custom/starter",
+	"kle1man/nvim",
+	require = "custom/starter/starter",
 	config = function()
 		require("starter.starter").setup({
 			header = "kle1man/nvim", -- changes default greeting
