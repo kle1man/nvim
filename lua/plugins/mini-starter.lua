f775949b8138727234a73c04f3bb76c00c479f4b
Alex Kleiman <alexjkleiman@gmail.com>
2024-02-24 17:48:08 -0500

    Temporarily disabled starter

--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -1,11 +1,13 @@
 return {
-    "echasnovski/mini.starter",
-    version = "*",
-    config = function()
-        require("mini.starter").setup({
-            header = "kle1man/nvim", -- changes default greeting
-            footer = "",    -- hides help section
-            silent = true,  -- hides query keystrokes
-        })
-    end,
+-- 	"echasnovski/mini.starter",
+-- 	version = "*",
+-- 	config = function()
+-- 		require("mini.starter").setup({
+-- 			header = "kle1man/nvim", -- changes default greeting
+-- 			footer = "", -- hides help section
+-- 			silent = true, -- hides query keystrokes
+-- 		})
+-- 		-- vim.cmd([[autocmd VimEnter * silent Neotree]])
+-- 		-- vim.cmd([[autocmd VimEnter * silent wincmd p]])
+-- 	end,
 }
