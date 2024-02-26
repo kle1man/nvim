96e0156f944a38402a3d3ced9ce748c15f3b21f2
Alex Kleiman <alexjkleiman@gmail.com>
2024-02-26 02:23:55 -0500

    Changed formatting

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,8 +1,8 @@
 return {
-    "cohama/lexima.vim",
-    config = function()
-        vim.cmd("call lexima#add_rule({'char': '$', 'input_after': '$', 'filetype': 'markdown'})")
-        vim.cmd("call lexima#add_rule({'char': '$', 'at': '\\%#\\$', 'leave': 1, 'filetype': 'markdown'})")
-        vim.cmd("call lexima#add_rule({'char': '<BS>', 'at': '\\$\\%#\\$', 'delete': 1, 'filetype': 'markdown'})")
-    end,
+	"cohama/lexima.vim",
+	config = function()
+		vim.cmd("call lexima#add_rule({'char': '$', 'input_after': '$', 'filetype': 'markdown'})")
+		vim.cmd("call lexima#add_rule({'char': '$', 'at': '\\%#\\$', 'leave': 1, 'filetype': 'markdown'})")
+		vim.cmd("call lexima#add_rule({'char': '<BS>', 'at': '\\$\\%#\\$', 'delete': 1, 'filetype': 'markdown'})")
+	end,
 }
