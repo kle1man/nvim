672433cddc55452527625c93b872d90b88e3a456
Alex Kleiman <alexjkleiman@gmail.com>
2024-02-25 20:16:25 -0500

    Implemented custom lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,63 @@
 return {
 	"nvim-lualine/lualine.nvim",
+	lazy = false,
 	config = function()
-		require("lualine").setup({})
+		-- Helper function to replace the "mode" module with the characters N/V/C/I rather than the full word
+		function check_neovim_mode()
+			local mode = vim.api.nvim_get_mode().mode
+			if mode == "n" then
+				return "N" -- normal mode
+			elseif mode == "v" or mode == "V" then
+				return "V" -- visual mode or v-line mode
+			elseif mode == "c" then
+				return "C" -- command-line mode
+			elseif mode == "i" then
+				return "I" -- insert mode
+			else
+				return "?" -- unknown mode
+			end
+		end
+
+		require("lualine").setup({
+			options = {
+				theme = {
+					normal = {
+						cterm = { bg = "none", fg = "none" },
+						gui = { bg = "none", fg = "none" },
+					},
+					insert = {
+						cterm = { bg = "none", fg = "none" },
+						gui = { bg = "none", fg = "none" },
+					},
+					visual = {
+						cterm = { bg = "none", fg = "none" },
+						gui = { bg = "none", fg = "none" },
+					},
+					replace = {
+						cterm = { bg = "none", fg = "none" },
+						gui = { bg = "none", fg = "none" },
+					},
+				},
+				icons_enabled = true,
+				component_separators = "",
+				section_separators = "",
+			},
+			sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { check_neovim_mode, "progress" },
+				lualine_x = { "diagnostics", "diff", "branch" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
 	end,
 }
